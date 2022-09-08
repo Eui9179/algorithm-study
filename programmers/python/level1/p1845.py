a600b3fdcd565e13af01f670e0195947cbd19671
eui9179 <outdark97@gmail.com>
2022-09-08 12:16:31 +0900

    feature/day3

---
+++ programmers/python/level1/p1845.py
@@ -0,0 +1,8 @@
+def solution(nums):
+    set_nums_len = len(set(nums))
+    nums_half_len = int(len(nums)/2)
+    
+    return min(set_nums_len, nums_half_len)
+
+nums = [3,1,2,3]
+print(solution(nums))
