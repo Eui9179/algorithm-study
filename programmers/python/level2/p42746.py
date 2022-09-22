f5e399b1c4f2c93ae9f27d2dd50476d86ad3a50a
Eui9179 <outdark97@gmail.com>
2022-09-23 00:36:47 +0900

    day11

--- programmers/python/level2/p42746.py
+++ programmers/python/level2/p42746.py
@@ -1,7 +1,6 @@
-# 정렬 - 가장 큰수
-# 다시 풀어야됨
+# 미해결
 
-def solution(numbers):# "1 11 110 1110"
+def solution(numbers):
     answer = ''
     
     num_map = {str(key):[] for key in range(9, -1, -1) }
@@ -9,7 +8,6 @@ def solution(numbers):# "1 11 110 1110"
         s_number = str(number)
         num_map[s_number[0]].append(s_number)
     
-    # 232 23
     for num_key in num_map:
         if len(num_map[num_key]) > 1:
             tmp = [num_map[num_key].pop(0)]
@@ -23,7 +21,7 @@ def solution(numbers):# "1 11 110 1110"
                     
                     t = tmp[i]
                     
-                    if len(num) < len(t):    
+                    if len(num) < len(t):
                         for j in range(1, len(num)):
                             numj = int(num[j])
                             tj = int(t[j])
@@ -32,15 +30,23 @@ def solution(numbers):# "1 11 110 1110"
                                 tmp.insert(i, num)
                                 is_append = True
                                 break
+                        
+                        last_digit = int(t[len(num)])
+                          
                         if is_append:
                             pass
                         
-                        elif int(t[len(num)]) < int(t[0]) and  int(t[len(num)]) < t[1]:
+                        elif last_digit < int(num[0]):
                             tmp.insert(i, num)
                             is_append = True
-                        # 끝자리가 같을때 어떻게 처리?
                         
-                            
+                        elif last_digit == int(num[0]):
+                            for n in num:
+                                if last_digit < int(n):
+                                    tmp.insert(i, num)
+                                    is_append = True
+                                    break
+                    
                     elif  len(t) < len(num):
                         for j in range(1, len(t)):
                             numj = int(num[j])
@@ -51,16 +57,22 @@ def solution(numbers):# "1 11 110 1110"
                                 is_append = True
                                 break
                             
-                        if not is_append and int(num[len(t)]) > int(t[0]): # 들어올 값이 클때
-                                tmp.insert(i, num)
-                                is_append = True
-                                
-                        if not is_append and t[len(num)] == t[0]:
+                        last_digit = int(num[len(t)])
+                        if is_append:
+                            pass
+                            
+                        elif last_digit > int(t[0]): # 들어올 값이 클때
                             tmp.insert(i, num)
                             is_append = True
-                        
+                                
+                        elif last_digit == int(t[0]):
+                            for t_num in t:
+                                if last_digit > int(t_num):
+                                    tmp.insert(i, num)
+                                    is_append = True
+                                    break
+                            
                     elif len(t) == len(num):
-                        # t: 979, 978
                         if int(t) == int(num):
                             tmp.insert(i, num)
                             is_append = True
@@ -75,36 +87,32 @@ def solution(numbers):# "1 11 110 1110"
                                     is_append = True
                                     break
                                 
-                    if not is_append: # 아무데도 못들어갔다면
-                        tmp.append(num)
-                        is_append = True
-                        
-                print(tmp)
+                if not is_append:
+                    tmp.append(num)
+                    is_append = True
                     
             for t in tmp:
-                answer += t + ' '
+                answer += t
         else:
             for num in num_map[num_key]:
-                answer += num + ' '
+                answer += num
         
-    return answer
-    
-# [6, 10, 2]
+    i = 0
+    count = 0
+    while i < len(answer)-1:
+        if answer[i] != '0':
+            break
+        else:
+            count += 1
+        i += 1
+        
+    return answer[count:len(answer)]
 
-# 맨 앞자리가 9일때 가장 큼
-
-# if 앞자리가 같은데 다음 자리가 다르다면?
-# 다음수가 맨 앞자리보다 크고 그중 가장 큰수를 가져온다
 '''
 [232,23] : 23/232 : (23232)
 [212,21] : 212/21 : (21221)
 [70,0,0,0,0] : 70/0/0/0/0 : (700000)
+23, 232, 21, 212
 '''
-print(solution([232,23]))
 
-# 979 97 978 818 81 817
-
-a = ['1','2']
-
-atmp = a.insert(0,'2')
-btmp = a.insert(0,'3')
+print(solution([23, 232, 21, 212]))
