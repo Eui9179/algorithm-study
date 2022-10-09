35be1a65d2ef01be84da666b219efdd58b5a6717
Eui9179 <outdark97@gmail.com>
2022-10-09 22:05:20 +0900

    disjoint-sets

---
+++ book/graph/test.py
@@ -0,0 +1,41 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        return find_parent(parent, parent[x])
+    return x
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+# 노드의 개수
+v = 6
+parent = [0] * (v + 1) # 부모 테이블 초기화
+
+# 유니온 실행할 리스트
+unions = [(1, 4), (2, 3), (2, 4), (5, 6)]
+
+# 부모 테이블상에서, 부모를 자기 자신으로 초기화
+for i in range(1, v + 1):
+    parent[i] = i
+    
+# Union 연산 각각 수행
+for union in unions:
+    a, b = union[0], union[1]
+    union_parent(parent, a, b)
+
+# 각 원소가 속한 집합 출력하기
+print('각 원소가 속한 집합: ', end='')
+for i in range(1, v + 1):
+    print(find_parent(parent, i), end='')
+
+print()
+
+# 부모 테이블 내용 출력하기
+print('부모 테이블: ', end='')
+for i in range(1, v + 1):
+    print(parent[i], end=' ')
