d5108b6a6b0e0730aaefbb72906d92f409faba1e
Eui9179 <outdark97@gmail.com>
2023-02-15 00:30:07 +0900

    p330

--- java/coding-test/src/implementations/practice/P327.java
+++ java/coding-test/src/implementations/practice/P327.java
@@ -1,32 +1,21 @@
 package implementations.practice;
 import java.util.*;
 public class P327 {
-    public int solution(int[][] board, int[][] appleLocations, int[] times, String[] directions) {
+    public int solution(int[][] board, ArrayList<Tupple<Integer, Integer>> appleLocations, ArrayList<Tupple<Integer, String>> turns) {
         int timesIndex = 0;
         int currentA = 1, currentB = 1;
-        int[][] direction = new int[4][2];
         int currentDir = 0;
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
 
-        ArrayList<Pair> snake = new ArrayList<>();
-
-        direction[0][0] = 0;
-        direction[0][1] = 1;
-
-        direction[1][0] = 1;
-        direction[1][1] = 0;
-
-        direction[2][0] = 0;
-        direction[2][1] = -1;
-
-        direction[3][0] = -1;
-        direction[3][1] = 0;
+        ArrayList<Tupple<Integer, Integer>> snake = new ArrayList<>();
 
         board[currentA][currentB] = 1;
-        snake.add(new Pair(currentA, currentB));
+        snake.add(new Tupple<>(currentA, currentB));
 
-        for (int i = 0; i < appleLocations.length; i++) {
-            board[appleLocations[i][0]][appleLocations[i][1]] = 5;
-        }
+        appleLocations.iterator().forEachRemaining(
+                apple -> board[apple.a][apple.b] = 5
+        );
 
         for (int time = 0; ; time++) {
             if (currentA == 0 || currentB == 0 || currentA == board.length || currentB == board.length) {
@@ -37,21 +26,21 @@ public class P327 {
 
             if (board[currentA][currentB] == 5) {
                 board[currentA][currentB] = 1;
-                snake.add(new Pair(currentA, currentB));
+                snake.add(new Tupple<>(currentA, currentB));
             } else if (board[currentA][currentB] == 0) {
                 board[currentA][currentB] = 1;
-                Pair tailPair = snake.remove(0);
+                Tupple<Integer, Integer> tailPair = snake.remove(0);
                 board[tailPair.a][tailPair.b] = 0;
-                snake.add(new Pair(currentA, currentB));
+                snake.add(new Tupple<>(currentA, currentB));
             }
 
-            if (timesIndex < times.length && time == times[timesIndex]) {
-                if (directions[timesIndex].equals("D")) {
+            if (timesIndex < turns.size() && time == turns.get(timesIndex).a) {
+                if (turns.get(timesIndex).b.equals("D")) {
                     currentDir++;
                     if (currentDir == 4) {
                         currentDir = 0;
                     }
-                } else if (directions[timesIndex].equals("L")) {
+                } else if (turns.get(timesIndex).b.equals("L")) {
                     currentDir--;
                     if (currentDir < 0) {
                         currentDir = 3;
@@ -59,8 +48,8 @@ public class P327 {
                 }
                 timesIndex++;
             }
-            currentA += direction[currentDir][0];
-            currentB += direction[currentDir][1];
+            currentA += dx[currentDir];
+            currentB += dy[currentDir];
         }
     }
 
@@ -74,35 +63,32 @@ public class P327 {
         int ax, ay, t;
         String d;
 
-        int[][] appleLocations = new int[k][2];
+        ArrayList<Tupple<Integer, Integer>> appleLocations = new ArrayList<>();
 
         for (int i = 0; i < k; i++) {
             ax = sc.nextInt();
             ay = sc.nextInt();
-            appleLocations[i][0] = ax;
-            appleLocations[i][1] = ay;
+            appleLocations.add(new Tupple<>(ax, ay));
         }
 
         int l = sc.nextInt();
 
-        int[] times = new int[l];
-        String[] directions = new String[l];
+        ArrayList<Tupple<Integer, String>> turns = new ArrayList<>();
 
         for (int i = 0; i < l; i++) {
             t = sc.nextInt();
             d = sc.next();
-            times[i] = t;
-            directions[i] = d;
+            turns.add(new Tupple<>(t, d));
         }
         P327 main = new P327();
-        System.out.println(main.solution(board, appleLocations, times, directions));
+        System.out.println(main.solution(board, appleLocations, turns));
     }
 
-    class Pair {
-        public int a;
-        public int b;
+    static class Tupple <T, V>{
+        public T a;
+        public V b;
 
-        public Pair(int a, int b) {
+        public Tupple(T a, V b) {
             this.a = a;
             this.b = b;
         }
