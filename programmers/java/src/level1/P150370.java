d0f412b1b6960be6fad243443ff1d72fe817038c
Eui9179 <outdark97@gmail.com>
2023-02-21 13:24:37 +0900

    programmers-java

--- programmers/java/src/level1/P150370.java
+++ programmers/java/src/level1/P150370.java
@@ -1,4 +1,4 @@
-package programmers;
+package level1;
 
 import java.util.*;
 
@@ -55,8 +55,8 @@ public class P150370 {
 
     public static void main(String[] args) {
         new P150370().solution("2022.05.19", new String[]{"A 6", "B 12", "C 3"}, new String[]{"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"});
-//        new P150370().solution("2021.12.08", new String[]{"A 18"}, new String[]{"2020.06.08 A"});
-//        new P150370().solution(
+//        new level1.P150370().solution("2021.12.08", new String[]{"A 18"}, new String[]{"2020.06.08 A"});
+//        new level1.P150370().solution(
 //                "2020.01.01",
 //                new String[]{"Z 3", "D 5"},
 //                new String[]{"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"}
