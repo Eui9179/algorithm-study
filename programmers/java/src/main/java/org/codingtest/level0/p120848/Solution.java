4a093bf91b9ac145f27e571e19088bbeefa9950e
Eui9179 <outdark97@gmail.com>
2023-02-26 00:58:49 +0900

    author-test

--- programmers/java/src/main/java/org/codingtest/level0/p120848/Solution.java
+++ programmers/java/src/main/java/org/codingtest/level0/p120848/Solution.java
@@ -3,8 +3,8 @@ package org.codingtest.level0.p120848;
 class Solution {
     public int solution(int n) {
         int preValue = 1;
-        int i = 2;
 
+        int i = 2;
         while (true) {
             preValue *= preValue * i;
             if (preValue > n) {
