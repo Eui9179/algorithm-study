a028a399cec499534a0440bdcda45e7178b9616b
Eui9179 <outdark97@gmail.com>
2023-03-17 11:06:09 +0900

    level1-page2

---
+++ programmers/java/src/main/java/org/codingtest/level1/p12969/Solution.java
@@ -0,0 +1,14 @@
+package org.codingtest.level1.p12969;
+
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for (int i = 0; i < b; i++) {
+            System.out.println("*".repeat(a));
+        }
+    }
+}
