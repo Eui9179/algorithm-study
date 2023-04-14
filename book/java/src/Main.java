45598d9c6ca0e698ad4516c11e811f35f1da210f
Eui9179 <outdark97@gmail.com>
2023-04-14 16:23:12 +0900

    union-find-알고리즘 및 사이클발생

--- book/java/src/Main.java
+++ book/java/src/Main.java
@@ -1,11 +1,17 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        Long i = 10L;
-        String si = i.toString();
-        String ss = String.valueOf(i);
 
-        System.out.println(si);
-        System.out.println(ss);
-
+        HashSet<String> set = new HashSet<>();
+        LinkedHashSet<String> set2 = new LinkedHashSet<>();
+        ArrayList;
+        LinkedList
     }
 }
+
+
+
