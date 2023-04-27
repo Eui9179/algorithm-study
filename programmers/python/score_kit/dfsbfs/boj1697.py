61bb673a4d9878cde8210d8e2ece982ad048aae8
Eui9179 <outdark97@gmail.com>
2023-04-27 10:07:01 +0900

    boj1697

---
+++ programmers/python/score_kit/dfsbfs/boj1697.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+
+def bfs(n, k, dist):
+    q = deque([n])
+
+    visited = [False] * 100001
+    moves = ['+', '-', '*']
+
+    while q:
+        node = q.popleft()
+
+        if node == k:
+            break
+        if visited[node]:
+            continue
+
+        visited[node] = True
+
+        for move in moves:
+            if move == '+':
+                nn = node + 1
+            elif move == '-':
+                nn = node - 1
+            else:
+                nn = node * 2
+
+            if nn < 0 or nn > 100000 or dist[nn] > 0:
+                continue
+
+            dist[nn] = dist[node] + 1
+
+            q.append(nn)
+
+
+n, k = map(int, input().split())
+
+dist = [0] * 100001
+bfs(n, k, dist)
+print(dist[k])
