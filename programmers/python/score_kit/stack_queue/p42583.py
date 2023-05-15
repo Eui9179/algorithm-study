8f31c9de5cb2721df4d80b5d5b70f0d685ef871a
Eui9179 <outdark97@gmail.com>
2023-05-15 13:43:00 +0900

    p42583-2

--- programmers/python/score_kit/stack_queue/p42583.py
+++ programmers/python/score_kit/stack_queue/p42583.py
@@ -1,11 +1,9 @@
 def solution(bridge_length, weight, truck_weights):
     bridge = []
     timer = []
-    result_count = 0
-    truck_count = len(truck_weights)
     time = 0
 
-    while result_count < truck_count:
+    while truck_weights or bridge:
         time += 1
 
         for i in range(len(timer)):
@@ -14,7 +12,6 @@ def solution(bridge_length, weight, truck_weights):
         while timer and timer[0] == 0:
             timer.pop(0)
             bridge.pop(0)
-            result_count += 1
 
         if truck_weights and len(bridge) < bridge_length and sum(bridge) + truck_weights[0] <= weight:
             truck = truck_weights.pop(0)
