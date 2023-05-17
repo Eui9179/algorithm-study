fdf9d5fdb2e0144394315e6ca42c683089ad3c82
Eui9179 <outdark97@gmail.com>
2023-05-17 18:08:27 +0900

    p12981

--- programmers/java/src/main/java/org/codingtest/level2/p12981/Solution.java
+++ programmers/java/src/main/java/org/codingtest/level2/p12981/Solution.java
@@ -3,15 +3,6 @@ package org.codingtest.level2.p12981;
 import java.util.*;
 
 class Solution {
-
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(new Solution().solution(
-                3,
-                new String[]{
-                        "tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"
-                })));
-    }
-
     public int[] solution(int n, String[] words) {
         int[] answer = {0, 0};
 
@@ -19,21 +10,27 @@ class Solution {
         wordSet.add(words[0]);
 
         for (int i = 1; i < words.length; i++) {
-            if (wordSet.contains(words[i])) {
-                answer[0] = (i % n) + 1;
-                answer[1] = (i / n) + 1;
-                break;
+
+            if (words[i].length() == 1) {
+                return calcIndex(i, n);
+
             }
+            if (wordSet.contains(words[i])) {
+                return calcIndex(i, n);
+            }
+
             wordSet.add(words[i]);
 
             if (words[i - 1].charAt(words[i - 1].length() - 1) !=
                     words[i].charAt(0)) {
-                answer[0] = (i % n) + 1;
-                answer[1] = ((i + 1) / n) + 1;
-                break;
+                return calcIndex(i, n);
             }
         }
 
-        return answer;
+        return new int[]{0, 0};
+    }
+
+    public int[] calcIndex(int i, int n) {
+        return new int[] {(i % n) + 1, (i / n) + 1};
     }
 }
