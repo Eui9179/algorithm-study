05e5ca51f7505dc45cd1aeb46f96aa5855db49bd
Eui9179 <outdark97@gmail.com>
2023-05-18 14:25:03 +0900

    p12980

--- programmers/java/src/main/java/org/codingtest/level2/p12985/Solution.java
+++ programmers/java/src/main/java/org/codingtest/level2/p12985/Solution.java
@@ -2,17 +2,18 @@ package org.codingtest.level2.p12985;
 
 class Solution {
     public int solution(int n, int a, int b) {
-        int answer = 0, parent = 0, d = n, t = 2, standard;
+        int answer = 0, preD = 0, d = n, t = 2, curD;
 
         while (d >= 1) {
             d = (n / t);
-            standard = d + parent;
+            curD = d + preD;
 
-            if (a > standard && b > standard) { // A, B가 모두 오른쪽 그룹에 속할 때
-                parent += d;
+            if (a > curD && b > curD) { // 오른쪽 그룹에 속할 때
+                preD += d;
             }
 
-            else if ((a <= standard && b > standard) || (a > standard && b <= standard)) {
+            else if ((a <= curD && b > curD) || (a > curD && b <= curD)) {
+                // curD기준으로 양쪽에 있을 때
                 answer = (int) (Math.log((double) d * 2) / Math.log(2));
                 break;
             }
