d31077e4304d42169e8e78dc3d59a2503d0c17ae
Eui9179 <outdark97@gmail.com>
2023-06-12 10:57:10 +0900

    b1015

---
+++ programmers/java/src/main/java/org/codingtest/boj/b1015/Main.java
@@ -0,0 +1,51 @@
+package org.codingtest.boj.b1015;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int e;
+        Node[] A = new Node[n];
+        String[] P = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            e = sc.nextInt();
+            A[i] = new Node(e, i);
+        }
+
+        Node[] B = Arrays.copyOf(A, n);
+        Arrays.sort(B);
+
+        for (int i = 0; i < n; i++) {
+            P[B[i].index] = String.valueOf(i);
+        }
+
+        /*
+        B[0] = A[2]
+        B[0] = A[B[0].index]
+
+        P[B[0].index] = 0;
+        B[P[i]] = A[i]
+
+        P[B[i].index] = i;
+         */
+
+        System.out.println(String.join(" ", P));
+    }
+
+    static class Node implements Comparable<Node> {
+        int num, index;
+
+        Node(int num, int index) {
+            this.num = num;
+            this.index = index;
+        }
+
+        @Override
+        public int compareTo(Node node) {
+            return this.num - node.num;
+        }
+    }
+}
