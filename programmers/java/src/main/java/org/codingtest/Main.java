fc969fa39c623f7d236d74b454939d0aaad63d8c
Eui9179 <outdark97@gmail.com>
2023-07-03 22:25:54 +0900

    p17686

--- programmers/java/src/main/java/org/codingtest/Main.java
+++ programmers/java/src/main/java/org/codingtest/Main.java
@@ -1,17 +1,6 @@
 package org.codingtest;
-import java.util.*;
-import java.util.stream.LongStream;
 
 public class Main {
     public static void main(String[] args) {
-        int nums = 12345;
-        int sum = 0;
-
-        while (nums != 0) {
-            sum += nums % 10;
-            nums /= 10;
-        }
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        System.out.println(sum);
     }
 }
