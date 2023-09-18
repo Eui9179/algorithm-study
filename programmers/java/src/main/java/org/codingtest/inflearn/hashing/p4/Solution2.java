40bd57450831affcfbcecc0169806519aab0a129
Eui9179 <outdark97@gmail.com>
2023-09-19 01:18:56 +0900

    hashing-p4

---
+++ programmers/java/src/main/java/org/codingtest/inflearn/hashing/p4/Solution2.java
@@ -0,0 +1,20 @@
+package org.codingtest.inflearn.hashing.p4;
+
+import java.util.*;
+
+class Solution2 {
+    public int solution(int[] nums, int m) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution2 T = new Solution2();
+        System.out.println(T.solution(new int[]{2, 2, 3, -1, -1, -1, 3, 1, 1}, 5));
+        System.out.println(T.solution(new int[]{1, 2, 3, -3, 1, 2, 2, -3}, 5));
+        System.out.println(T.solution(new int[]{1, 2, 3, -3, 1, 2}, 3));
+        System.out.println(T.solution(new int[]{-1, 0, 1}, 0));
+        System.out.println(T.solution(new int[]{-1, -1, -1, 1}, 0));
+    }
+}
