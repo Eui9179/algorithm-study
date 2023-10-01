ca2baa433fbcddcedb8310a10589a882da588fc8
Eui9179 <outdark97@gmail.com>
2023-10-02 00:12:12 +0900

    ds-p5-complete

--- programmers/java/src/main/java/org/codingtest/inflearn/ds/p5/Solution.java
+++ programmers/java/src/main/java/org/codingtest/inflearn/ds/p5/Solution.java
@@ -4,20 +4,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[][] tasks) {
-        int[] answer = {};
+        int[] answer = new int[tasks.length];
 
-        // 실행시간이 가장 작은 작업 먼저 처리
-        // 같은 경우 작업번호가 작은 것 먼저 처리
-        // 5초에 끝나면 5초에 실행
-
+        PriorityQueue<Program> sortedPrograms = new PriorityQueue<>();
         PriorityQueue<Task> taskQueue = new PriorityQueue<>();
+
         int time = 0;
+        int index = 0;
 
         for (int i = 0; i < tasks.length; i++) {
-            taskQueue.add(new Task(i, tasks[i][0], tasks[i][1]));
+            sortedPrograms.offer(new Program(i, tasks[i][0], tasks[i][1]));
         }
 
-        
+        while (!sortedPrograms.isEmpty() || !taskQueue.isEmpty()) {
+            while (!sortedPrograms.isEmpty() && time >= sortedPrograms.peek().callTime) {
+                taskQueue.offer(Task.of(sortedPrograms.poll()));
+            }
+
+            if (taskQueue.isEmpty()) {
+                time++;
+                continue;
+            }
+
+            Task task = taskQueue.poll();
+            time += task.executeTime;
+
+            answer[index] = task.index;
+            index++;
+        }
 
         return answer;
     }
@@ -25,24 +39,24 @@ class Solution {
     public static void main(String[] args) {
         Solution T = new Solution();
         System.out.println(Arrays.toString(T.solution(new int[][]{{2, 3}, {1, 2}, {8, 2}, {3, 1}, {10, 2}})));
-//        System.out.println(Arrays.toString(T.solution(new int[][]{{5, 2}, {7, 3}, {1, 3}, {1, 5}, {2, 2}, {1, 1}})));
-//        System.out.println(Arrays.toString(T.solution(new int[][]{{1, 2}, {2, 3}, {1, 3}, {3, 3}, {8, 2}, {1, 5}, {2, 2}, {1, 1}})));
-//        System.out.println(Arrays.toString(T.solution(new int[][]{{999, 1000}, {996, 1000}, {998, 1000}, {999, 7}})));
+        System.out.println(Arrays.toString(T.solution(new int[][]{{5, 2}, {7, 3}, {1, 3}, {1, 5}, {2, 2}, {1, 1}})));
+        System.out.println(Arrays.toString(T.solution(new int[][]{{1, 2}, {2, 3}, {1, 3}, {3, 3}, {8, 2}, {1, 5}, {2, 2}, {1, 1}})));
+        System.out.println(Arrays.toString(T.solution(new int[][]{{999, 1000}, {996, 1000}, {998, 1000}, {999, 7}})));
     }
 
-    static class Task implements Comparable<Task> {
+    static class Program implements Comparable<Program> {
         int index;
         int callTime;
         int executeTime;
 
-        Task(int index, int callTime, int executeTime) {
+        Program(int index, int callTime, int executeTime) {
             this.index = index;
             this.callTime = callTime;
             this.executeTime = executeTime;
         }
 
         @Override
-        public int compareTo(Task t) {
+        public int compareTo(Program t) {
             if (this.callTime == t.callTime) {
                 if (this.executeTime == t.executeTime) {
                     return this.index - t.index;
@@ -51,14 +65,27 @@ class Solution {
             }
             return this.callTime - t.callTime;
         }
+    }
+
+    static class Task implements Comparable<Task> {
+        int index;
+        int executeTime;
+
+        Task(int index, int executeTime) {
+            this.index = index;
+            this.executeTime = executeTime;
+        }
+
+        static Task of(Program program) {
+            return new Task(program.index, program.executeTime);
+        }
 
         @Override
-        public String toString() {
-            return "{" +
-                    "index=" + index +
-                    ", callTime=" + callTime +
-                    ", executeTime=" + executeTime +
-                    '}';
+        public int compareTo(Task t) {
+            if (this.executeTime == t.executeTime) {
+                return this.index - t.index;
+            }
+            return this.executeTime - t.executeTime;
         }
     }
 }
