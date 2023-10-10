3f6e91865be24a2891a980c95a556f3a230442a3
Eui9179 <outdark97@gmail.com>
2023-10-11 00:57:34 +0900

    st-p6-refactoringn

--- programmers/java/src/main/java/org/codingtest/inflearn/st/p6/Solution.java
+++ programmers/java/src/main/java/org/codingtest/inflearn/st/p6/Solution.java
@@ -4,49 +4,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] tasks, long k) {
-        int answer = 0;
+        int[] sT = copy(tasks);
+        int size = tasks.length;
+        int lastT = 0;
 
-        int[][] sorted = copyWithIndex(tasks);
-        int queueIndex = 0;
-        int queueSize = sorted.length;
-        Arrays.sort(sorted, Comparator.comparingInt(s -> s[1]));
+        Arrays.sort(sT);
 
-        int preValue = sorted[0][1];
-
-        while ((long) sorted[queueIndex][1] * queueSize <= k) {
-            k -= (long) sorted[queueIndex][1] * queueSize;
-            sorted[queueIndex][1] = 0;
-            queueIndex++;
-            if (queueIndex < sorted.length) {
-                int temp = sorted[queueIndex][1];
-                sorted[queueIndex][1] -= preValue;
-                preValue = temp;
+        for (int i = 1; i < sT.length; i++) {
+            long cycle = (long)(sT[i] - sT[i - 1]) * size;
+            if (cycle <= k) {
+                k -= cycle;
+                size--;
+            } else {
+                lastT = sT[i];
+                break;
             }
-            queueSize--;
         }
 
-
-        for (int[] s : sorted) {
-            tasks[s[0]] = s[1];
-        }
-
-        k %= queueSize;
+        k %= size;
         if (k == 0) return tasks.length;
 
         for (int i = 0; i < tasks.length; i++) {
-            if (tasks[i] == 0) continue;
+            if (tasks[i] < lastT) continue;
             if (k == 0) return i + 1;
             k--;
         }
-
         return 0;
     }
 
-    public int[][] copyWithIndex(int[] tasks) {
-        int[][] result = new int[tasks.length][2];
+    public int[] copy(int[] tasks) {
+        int[] result = new int[tasks.length + 1];
         for (int i = 0; i < tasks.length; i++) {
-            result[i][0] = i;
-            result[i][1] = tasks[i];
+            result[i + 1] = tasks[i];
         }
         return result;
     }
