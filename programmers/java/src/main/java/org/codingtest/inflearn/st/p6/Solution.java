58e8a8d243251376c1d66aa3a88c8a40f519c9c8
Eui9179 <outdark97@gmail.com>
2023-10-19 23:41:34 +0900

    refactoring-st-p6

--- programmers/java/src/main/java/org/codingtest/inflearn/st/p6/Solution.java
+++ programmers/java/src/main/java/org/codingtest/inflearn/st/p6/Solution.java
@@ -4,13 +4,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] tasks, long k) {
-        int[] sT = copy(tasks);
+        int[] sT = copy(tasks); // 앞에 0을 넣고 배열 복사
         int size = tasks.length;
         int lastT = 0;
 
         Arrays.sort(sT);
 
         for (int i = 1; i < sT.length; i++) {
+            // 가장 작은 값부터 사이클을 체크하며 순환
             long cycle = (long)(sT[i] - sT[i - 1]) * size;
             if (cycle <= k) {
                 k -= cycle;
@@ -25,18 +26,17 @@ class Solution {
         if (k == 0) return tasks.length;
 
         for (int i = 0; i < tasks.length; i++) {
-            if (tasks[i] < lastT) continue;
+            if (tasks[i] < lastT) continue; // 마지막으로 처리했던 task 값보다 작으면 무시(이미 0으로 변경됨)
             if (k == 0) return i + 1;
             k--;
         }
+
         return 0;
     }
 
     public int[] copy(int[] tasks) {
         int[] result = new int[tasks.length + 1];
-        for (int i = 0; i < tasks.length; i++) {
-            result[i + 1] = tasks[i];
-        }
+        System.arraycopy(tasks, 0, result, 1, tasks.length);
         return result;
     }
 
