c6b73693378dab521ab320eb8eeb4580cbca4482
Eui9179 <outdark97@gmail.com>
2025-10-06 01:28:14 +0900

    p227

--- book/java/src/y25/dynamic/p227.java
+++ book/java/src/y25/dynamic/p227.java
@@ -1,4 +1,31 @@
 package y25.dynamic;
 
+import java.util.Arrays;
+
 public class p227 {
+    public static void main(String[] args) {
+        System.out.println(solution(15, new int[]{2, 3}));
+        System.out.println(solution(4, new int[]{3, 5, 7}));
+    }
+
+    private static int solution(int m, int[] kinds) {
+        int[] dp = new int[m + 1];
+        Arrays.fill(dp, -1);
+
+        for (int k : kinds) {
+            if (k <= m) dp[k] = 1;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int k : kinds) {
+                if (i - k < 0 || dp[i - k] == -1) continue;
+                min = Math.min(min, dp[i - k]);
+            }
+            if (min != Integer.MAX_VALUE)
+                dp[i] = min + 1;
+        }
+
+        return dp[m];
+    }
 }
