8587068e10a75c7aa418fdfbfb1fdd94c623e6a7
Eui9179 <outdark97@gmail.com>
2025-12-21 23:18:01 +0900

    P77486

---
+++ book/java/src/y25/level1/P77486.java
@@ -0,0 +1,8 @@
+package y25.level1;
+
+public class P77486 {
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int[] answer = {};
+        return answer;
+    }
+}
